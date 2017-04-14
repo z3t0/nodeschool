6fb6c0cdc319783a6c6c5d8a8243cb3cad634988
Rafi Khan <zetoslab@gmail.com>
2017-04-13 19:13:29 -0600

    added http module
    learned:
    - http GET
    - listen to res for data
    - setEncoding (utf8)

---
+++ learnyounode/http.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const url = process.argv[2]
+
+http.get(url, (res) => {
+
+    res.setEncoding('utf8')
+
+    res.on('data', (data) => {
+        console.log(data)
+    })
+
+})
+
