4838264f1a3753fbfe8cd18769bbc08c469b8ad5
Rafi Khan <zetoslab@gmail.com>
2017-04-13 20:19:07 -0600

    working on http.js
    learned:
    - can aggregate data from stream

--- learnyounode/http.js
+++ learnyounode/http.js
@@ -2,12 +2,19 @@ const http = require('http')
 
 const url = process.argv[2]
 
+var all = ""
+
 http.get(url, (res) => {
 
     res.setEncoding('utf8')
 
     res.on('data', (data) => {
-        console.log(data)
+        all += data
+    })
+
+    res.on('end', () => {
+        console.log(all.length)
+        console.log(all)
     })
 
 })
